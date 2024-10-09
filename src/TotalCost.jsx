937742baac7971922f116a5f33c85bae4d4fe585
FloSpin <florianspinoza@gmail.com>
2024-10-09 15:33:55 -0400

    final fix

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
+
+
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
+
+    const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
+
     return (
         <div className="pricing-app">
             <div className="display_box">
@@ -8,7 +13,7 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
                     <p className="preheading"><h3>Total cost for the event</h3></p>
                 </div>
                 <div>
-                    <h2 id="pre_fee_cost_display" className="price"></h2>
+                    <h2 id="pre_fee_cost_display" className="price">${total_amount}</h2>
                     <div className="render_items"></div>
                 </div>
                 <div className="render_items">
